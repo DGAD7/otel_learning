d4974ebe863400415d87051d89f7edf029fd8b75
Darshan Gad <darshangad@gmail.com>
2026-09-20 16:47:14 +0200

    setup proto files

---
+++ scripts/generate_protobuf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -x
+
+SCRIPT_PATH=$(dirname "$0")
+ROOT_PATH=$(realpath "$SCRIPT_PATH/..")
+
+PROTO_FILES="proto/*.proto"
+
+cd "$ROOT_PATH"
+
+CPP_OUT="$ROOT_PATH"/control/grpc/generated
+mkdir -p "$CPP_OUT" ui/grpc/generated
+
+PROTOC=$(which protoc)
+# Generate for controller
+$PROTOC -I=./proto \
+	--cpp_out=$CPP_OUT \
+	--grpc_out=$CPP_OUT \
+	--plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) \
+	$PROTO_FILES
+
+# Generate for UI
+$PROTOC -I=./proto \
+	--go_out=. \
+	--go-grpc_out=. \
+	--go_opt=paths=import \
+	$PROTO_FILES
